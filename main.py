a44235af101064fdfea45d2a17f5e78b7fa987c5
Yamavu <yamavu@gmail.com>
2025-06-25 21:17:32 +0200

    cleanup and splitting

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ from argparse import ArgumentParser
 from itertools import cycle
 from pathlib import Path
 
-from pygame import Surface, init as pygame_init, quit as pygame_quit, Rect
+from pygame import Surface, Rect
+from pygame import init as pygame_init, quit as pygame_quit
 from pygame import QUIT, KEYDOWN, K_ESCAPE, SRCALPHA
 from pygame.transform import smoothscale
 from pygame.image import load
@@ -12,10 +13,7 @@ from pygame.time import Clock, get_ticks
 from pygame.display import set_mode, flip
 from pygame.event import get as get_events
 
-IMAGE_SUFFIXES = [".jpg", ".png", ".jpeg", ".bmp"]
-
-TARGET = Rect(100,40,400,400)
-TARGET_RATIO = TARGET.width / TARGET.height
+from transition import transition_fade
 
 def images_iter(folder:Path) -> Iterator[Path]:
     if not folder.is_dir():
@@ -42,40 +40,32 @@ def scale_image(image_path:Path, rect:Rect) -> Surface:
     print(f"scaling {image_path.stem} from {(_width, _height)} to {_target}")
     return smoothscale(s, _target)
 
-def iter_slideshow_images(folder:Path):
+def iter_slideshow_images(folder:Path, target:Rect, image_suffixes:Tuple[str]):
     for image_path in images_iter(folder):
-        if image_path.suffix.lower() not in IMAGE_SUFFIXES:
+        if image_path.suffix.lower() not in image_suffixes:
             continue
-        target_surface = Surface(TARGET.size, flags=SRCALPHA)
-        _scaled_image = scale_image(image_path, TARGET)
+        target_surface = Surface(target.size, flags=SRCALPHA)
+        _scaled_image = scale_image(image_path, target)
         _target_rect = _scaled_image.get_rect(center=target_surface.get_rect().center)
         target_surface.blit(_scaled_image, _target_rect)
         # draw _s in the center of _target_surface
         yield target_surface
 
-def draw_background(screen:Surface) -> Surface:
+def draw_background(screen:Surface) -> None:
     screen.fill((128, 32, 192))
 
-def transition_fade(screen:Surface, cur_img:Surface, next_img:Surface, progress:float):
-    def linear(p) -> int:
-        return int(p *  255)
-    def quadratic(p) -> int:
-        return int(p * p * 255)
-    _cur_img = cur_img.copy()
-    _cur_img.set_alpha(linear(1.0-progress))
-    _next_img = next_img.copy()
-    _next_img.set_alpha(linear(progress))
-    screen.blit(_cur_img, dest=TARGET.topleft)
-    screen.blit(_next_img, dest=TARGET.topleft)
+def draw_foreground(screen:Surface) -> None:
+    pass
 
-def main(folder:Path):
+def main(folder:Path, image_display_time:int, transition_time:int):
     pygame_init()
-    transition_duration: int = 1000
-    image_display_time: int = 3000
     screen_size: Tuple[int,int] = (640, 480)
+    target = Rect(100,40,400,400)
+    image_suffixes = (".jpg", ".png", ".jpeg", ".bmp")
+    #target_ratio = target.width / target.height
     clock = Clock()
     screen = set_mode(screen_size)
-    _iter = iter_slideshow_images(folder)
+    _iter = iter_slideshow_images(folder, target, image_suffixes)
     cur_img = next(_iter, None)
     #current_image = next(image_iter, None)
     nxt_img = next(_iter, None)
@@ -98,7 +88,7 @@ def main(folder:Path):
             progress = 0.0
         draw_background(screen)
         if in_transition:
-            progress = (now - transistion_start) / transition_duration
+            progress = (now - transistion_start) / transition_time
             if progress >= 1.0:
                 # End of fade
                 cur_img = nxt_img
@@ -106,9 +96,10 @@ def main(folder:Path):
                 last_switch = now
                 in_transition = False
                 progress = 0.0
-            transition_fade(screen, cur_img, nxt_img, progress)
+            transition_fade(screen, cur_img, nxt_img, progress, target)
         else:
-            screen.blit(cur_img, TARGET.topleft)
+            screen.blit(cur_img, target.topleft)
+        draw_foreground(screen)
         flip()
         clock.tick(60)
     pygame_quit()
@@ -117,5 +108,11 @@ def main(folder:Path):
 if __name__ == "__main__":
     p = ArgumentParser()
     p.add_argument("folder", type=Path, default=Path("img"), nargs="?")
+    p.add_argument("image_display_time", type=float, default=4.0, nargs="?")
+    p.add_argument("transition_time", type=float, default=1.0, nargs="?")
     args = p.parse_args()
-    main(folder=args.folder)
+    main(
+        folder = args.folder, 
+        image_display_time = int(args.image_display_time * 1000),
+        transition_time = int(args.transition_time * 1000)
+    )
--- transition.py
+++ transition.py
@@ -1,1 +1,15 @@
-from pygame import Surface
+from pygame import Surface, Rect
+
+def linear(p) -> int:
+    return int(p *  255)
+
+def polynomial(p, x) -> int:
+    return int(pow(p,x) * 255)
+
+def transition_fade(screen:Surface, cur_img:Surface, next_img:Surface, progress:float, target:Rect):
+    _cur_img = cur_img.copy()
+    _cur_img.set_alpha(polynomial(1.0-progress, 4))
+    _next_img = next_img.copy()
+    _next_img.set_alpha(linear(progress))
+    screen.blit(_cur_img, dest=target.topleft)
+    screen.blit(_next_img, dest=target.topleft)
